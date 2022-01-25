b08a250d73377f0239954ea997f84ebca6282cf1
iheaven0129@gmail.com <*Dbtldud12345>
2022-01-25 00:54:05 -0500

    Initiated JPA codes

---
+++ src/main/java/com/knockknock/yoo/springboot/domain/posts/Posts.java
@@ -0,0 +1,47 @@
+package com.knockknock.yoo.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
---
+++ src/test/java/com/knockknock/yoo/springboot/domain/posts/PostsTest.java
@@ -0,0 +1,7 @@
+package com.knockknock.yoo.springboot.domain.posts;
+
+import static org.junit.Assert.*;
+
+public class PostsTest {
+
+}
