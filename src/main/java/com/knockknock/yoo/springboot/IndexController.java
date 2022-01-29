7e262096f2ddca539b0648c87d3ea20532f507a6
knockknockyoo <iheaven0129@gmail.com>
2022-01-29 14:11:18 -0500

    Used mustache template to map server side dto Implemented listing posts

--- src/main/java/com/knockknock/yoo/springboot/IndexController.java
+++ src/main/java/com/knockknock/yoo/springboot/IndexController.java
@@ -1,14 +1,21 @@
 package com.knockknock.yoo.springboot;
 
+import com.knockknock.yoo.springboot.domain.posts.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final PostService postService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model)
+    {
+        model.addAttribute("posts", postService.findAllDesc());
         return "index";
     }
 
--- src/main/java/com/knockknock/yoo/springboot/web/dto/PostsListResponseDto.java
+++ src/main/java/com/knockknock/yoo/springboot/web/dto/PostsListResponseDto.java
@@ -1,7 +1,22 @@
 package com.knockknock.yoo.springboot.web.dto;
 
+import com.knockknock.yoo.springboot.domain.posts.Posts;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts posts) {
+        this.id = posts.getId();
+        this.title = posts.getTitle();
+        this.author = posts.getAuthor();
+        this.modifiedDate = posts.getModifiedDate();
+    }
 }
