d24309e82af7f5fae2af3f69ae3ce58a60f0cd12
knockknockyoo <iheaven0129@gmail.com>
2022-01-31 11:41:10 -0500

    Added Oauth2.0 Login

--- src/main/java/com/knockknock/yoo/springboot/IndexController.java
+++ src/main/java/com/knockknock/yoo/springboot/IndexController.java
@@ -1,21 +1,29 @@
 package com.knockknock.yoo.springboot;
 
+import com.knockknock.yoo.springboot.config.auth.SessionUser;
 import com.knockknock.yoo.springboot.domain.posts.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostService postService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model)
     {
         model.addAttribute("posts", postService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user !=null) model.addAttribute("userName", user.getName());
+
         return "index";
     }
 
--- src/main/java/com/knockknock/yoo/springboot/config/auth/SessionUser.java
+++ src/main/java/com/knockknock/yoo/springboot/config/auth/SessionUser.java
@@ -1,19 +1,19 @@
-//package com.knockknock.yoo.springboot.config.auth;
-//
-//import com.knockknock.yoo.springboot.domain.user.User;
-//import lombok.Getter;
-//
-//import java.io.Serializable;
-//
-//@Getter
-//public class SessionUser implements Serializable {
-//    private String name;
-//    private String email;
-//    private String picture;
-//
-//    public SessionUser(User user) {
-//        this.name=user.getName();
-//        this.email=user.getEmail();
-//        this.picture=user.getPicture();
-//    }
-//}
+package com.knockknock.yoo.springboot.config.auth;
+
+import com.knockknock.yoo.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name=user.getName();
+        this.email=user.getEmail();
+        this.picture=user.getPicture();
+    }
+}
